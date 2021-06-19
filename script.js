f1f25754dabeef7f2ca1975e7a97c1dd66a9af41
AL-HUSSEIN ABUBAKAR <al-hussein.abubakar@amalitech.org>
2021-06-19 23:34:46 +0000

    MFirst Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let button = document.getElementById("btn");
+
+
+
+function year(){
+	let input = document.getElementById("input").value;
+	if (input % 4 === 0) {
+		if (input % 100 === 0) {
+			if (input % 400 === 0) {
+				document.getElementById("result").innerHTML = input +" is a leap year";
+			} else {
+				document.getElementById("result").innerHTML = input+" is not a leap year";
+			}
+
+		} else {
+			document.getElementById("result").innerHTML = input+" is a leap year";
+		}
+
+	} else {
+		document.getElementById("result").innerHTML = input+" is not leap year";
+
+	}
+
+}
+
