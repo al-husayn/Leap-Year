824ca5ad548f02e58493c5ff11be2284ea32317a
AL-HUSSEIN ABUBAKAR <al-hussein.abubakar@amalitech.org>
2021-06-20 08:22:35 +0000

    Formatted strings

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-
 let button = document.getElementById("btn");
 
-
-button.addEventListener("click", ()=>{
+button.addEventListener("click", () => {
 	let input = document.getElementById("input").value;
 	if (input % 4 === 0) {
 		if (input % 100 === 0) {
 			if (input % 400 === 0) {
-				document.getElementById("result").innerHTML = input +" is a leap year";
+				document.getElementById("result").innerHTML = `${input}  is a leap year`;
 			} else {
-				document.getElementById("result").innerHTML = input+" is not a leap year";
+				document.getElementById("result").innerHTML = `${input} is not a leap year`;
 			}
 
 		} else {
-			document.getElementById("result").innerHTML = input+" is a leap year";
+			document.getElementById("result").innerHTML = `${input} is a leap year`;
 		}
 
 	} else {
-		document.getElementById("result").innerHTML = input+" is not leap year";
+		document.getElementById("result").innerHTML = `${input} is not leap year`;
 
 	}
-	
-}, false);
 
+}, false);
